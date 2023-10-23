564a491c8919d2ebedb8183bf0b35e4fdba13348
alexroig <alex.roigronda@cieep.com>
2023-10-23 11:24:49 +0200

    por si aca

--- app/src/main/java/alex/roig/ejercicioinmobiliaria/MainActivity.java
+++ app/src/main/java/alex/roig/ejercicioinmobiliaria/MainActivity.java
@@ -1,6 +1,12 @@
 package alex.roig.ejercicioinmobiliaria;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import alex.roig.ejercicioinmobiliaria.databinding.ActivityMainBinding;
@@ -9,6 +15,7 @@ import alex.roig.ejercicioinmobiliaria.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private ActivityResultLauncher<Intent> launcherAddInmueble;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +24,27 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
+        inicializarLaunchers();
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                launcherAddInmueble.launch(new Intent(MainActivity.this, AddInmuebleActivity.class));
             }
         });
+    }
+
+    private void inicializarLaunchers() {
+
+
+
+        launcherAddInmueble = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
+                        //RESULTADO DE VOLVER DE LA ACTIVIDAD ADD INMUEBLE
+                    }
+                }
+        );
     }
 }
